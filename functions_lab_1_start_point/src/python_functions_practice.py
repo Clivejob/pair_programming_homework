27eed5ac41690523ff8f050d6c4f60de5c999335
Lewis Brennan <lewisbrennan444@gmail.com>
2023-03-01 20:53:27 +0000

    initial commit

--- functions_lab_1_start_point/src/python_functions_practice.py
+++ functions_lab_1_start_point/src/python_functions_practice.py
@@ -1,20 +1,49 @@
 def return_10():
     return 10
 
-def add(num1, num2):
-    add_result = num1 + num2
-    return add_result
+def add(int_one, int_two):
+    sum = int_one + int_two
+    return sum
 
-def subtract(num1, num2):
-    subtract_result = num1 - num2
-    return subtract_result
+def subtract(int_one, int_two):
+    sum = int_one - int_two
+    return sum
 
-def length_of_string(string):
-    length = len(string)
-    return length
+def multiply(int_one, int_two):
+    sum = int_one * int_two
+    return sum
 
-def join_string(string_1, string_2):
-    return string_1 + string_2
+def divide(int_one, int_two):
+    sum = int_one / int_two
+    return sum
 
-def add_string_as_number():
-    return int("1") + int("2")
+def length_of_string(string_example):
+    return len(string_example)
+
+def join_string(string_ex_1, string_ex_2):
+    return (f"{string_ex_1}{string_ex_2}")
+
+def add_string_as_number(str_one, str_two):
+    sum = 0
+    sum = (int(str_one) + int(str_two))
+    return sum
+
+def number_to_full_month_name(number_month):
+    typed_month = None
+    if number_month == 1:
+        typed_month = "January"
+    elif number_month == 3:
+    	typed_month = "March"
+    elif number_month == 9:
+    	typed_month = "September"
+    return typed_month
+
+def number_to_short_month_name(number_month):
+    typed_month = None
+    if number_month == 1:
+        typed_month = "Jan"
+    elif number_month == 4:
+    	typed_month = "Apr"
+    elif number_month == 10:
+    	typed_month = "Oct"
+    return typed_month
